e3876968038c200b0408c1aaef1ba4573ab6bdb2
Samrah-Waheed <samrahwaheed484@gmail.com>
2025-12-03 18:34:16 +0500

    Update MagicButton.tsx

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -1,6 +1,13 @@
-"use client";
 import React from "react";
 
+/**
+ *  UI: border magic from tailwind css btns
+ *  Link: https://ui.aceternity.com/components/tailwindcss-buttons
+ *
+ *  change border radius to rounded-lg
+ *  add margin of md:mt-10
+ *  remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
+ */
 const MagicButton = ({
   title,
   icon,
@@ -10,26 +17,21 @@ const MagicButton = ({
 }: {
   title: string;
   icon: React.ReactNode;
-  position: "left" | "right";
+  position: string;
   handleClick?: () => void;
   otherClasses?: string;
 }) => {
   return (
     <button
+      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
       onClick={handleClick}
-      className={`relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg focus:outline-none`}
     >
-      {/* Glowing rotating border — ORIGINAL UI */}
-      <span
-        className="absolute inset-[-150%] animate-spin
-        bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"
-      ></span>
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
-      {/* Inner Button */}
+      {/* remove px-3 py-1, add px-5 gap-2 */}
       <span
-        className={`inline-flex h-full w-full items-center justify-center rounded-lg
-        bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2
-        relative ${otherClasses}`}
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
       >
         {position === "left" && icon}
         {title}
